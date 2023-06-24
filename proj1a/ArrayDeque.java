4dd205be03a35c63889de976659afacb7d84e5a5
Felipe Lima <26683783+feslima@users.noreply.github.com>
2023-06-24 20:35:42 -0300

    feat: proj1a ArrayDeque

---
+++ proj1a/ArrayDeque.java
@@ -0,0 +1,175 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayDeque<T> {
+    private int size;
+    private int nextFirst;
+    private int nextLast;
+    private T[] items;
+    private int capacity;
+
+    public ArrayDeque() {
+        size = 0;
+        capacity = 8;
+
+        items = (T[]) new Object[capacity];
+        nextFirst = 0; // moves to the left (decrements) on addFirst
+        nextLast = nextFirst + 1; // moves to the right (increments) on addLast
+    }
+
+    private void copyArray(T[] oldItems, T[] newItems) {
+        int counter = 0;
+        int pointer = (nextFirst + 1) % oldItems.length;
+        while (counter < size) {
+            newItems[counter] = oldItems[pointer];
+            counter += 1;
+            pointer = (pointer + 1) % oldItems.length;
+        }
+    }
+
+    private void expand() {
+        capacity = capacity * 2;
+        T[] oldItems = items;
+        items = (T[]) new Object[capacity];
+
+        copyArray(oldItems, items);
+
+        nextFirst = items.length - 1;
+        nextLast = size;
+    }
+
+    private void downsize() {
+        capacity = capacity / 2;
+        T[] oldItems = items;
+        items = (T[]) new Object[capacity];
+
+        copyArray(oldItems, items);
+
+        nextFirst = items.length - 1;
+        nextLast = size;
+    }
+
+    private double getUsageRatioThreshold() {
+        return 0.25;
+    }
+
+    private double getUsageRatio() {
+        return size * 1.0 / items.length;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public T get(int index) {
+        if (size == 0) {
+            return null;
+        }
+        int previousFirst = (nextFirst + 1) % items.length;
+        int readIndex = (previousFirst + index) % items.length;
+        return items[readIndex];
+    }
+
+    public void addFirst(T item) {
+        if (size == items.length) {
+            expand();
+        }
+
+        items[nextFirst] = item;
+        nextFirst -= 1;
+
+        if (nextFirst < 0) {
+            nextFirst = items.length - 1;
+        }
+
+        size += 1;
+    }
+
+    public void addLast(T item) {
+        if (size == items.length) {
+            expand();
+        }
+
+        items[nextLast] = item;
+        nextLast = (nextLast + 1) % items.length;
+
+        size += 1;
+    }
+
+    public T removeFirst() {
+        if (size == 0) {
+            return null;
+        }
+
+        int previousFirst = (nextFirst + 1) % items.length;
+        T removedItem = items[previousFirst];
+        items[previousFirst] = null;
+        nextFirst = previousFirst;
+
+        size -= 1;
+
+        if (getUsageRatio() <= getUsageRatioThreshold()) {
+            downsize();
+        }
+
+        return removedItem;
+    }
+
+    public T removeLast() {
+        if (size == 0) {
+            return null;
+        }
+
+        int previousLast = nextLast - 1;
+        if (previousLast < 0) {
+            previousLast = items.length - 1;
+        }
+
+        T removedItem = items[previousLast];
+        items[previousLast] = null;
+        nextLast = previousLast;
+
+        size -= 1;
+
+        if (getUsageRatio() <= getUsageRatioThreshold()) {
+            downsize();
+        }
+
+        return removedItem;
+    }
+
+    public String getElements() {
+        if (size == 0) {
+            return "";
+        }
+        List<String> elements = new ArrayList<>();
+
+        int counter = 0;
+        int pointer = (nextFirst + 1) % items.length;
+        while (counter < size) {
+            elements.add(items[pointer].toString());
+            counter += 1;
+            pointer = (pointer + 1) % items.length;
+        }
+
+        return String.join(" | ", elements);
+    }
+
+    public void printDeque() {
+        if (size == 0) {
+            return;
+        }
+
+        int counter = 0;
+        int pointer = (nextFirst + 1) % items.length;
+        while (counter < size) {
+            System.out.println(items[pointer].toString());
+            counter += 1;
+            pointer = (pointer + 1) % items.length;
+        }
+    }
+}
---
+++ proj1a/ArrayDequeTest.java
@@ -0,0 +1,178 @@
+/**
+ * Performs some basic linked list tests.
+ */
+public class ArrayDequeTest {
+
+    /* Utility method for printing out empty checks. */
+    public static boolean checkEmpty(boolean expected, boolean actual) {
+        if (expected != actual) {
+            System.out.println("isEmpty() returned " + actual + ", but expected: " + expected);
+            return false;
+        }
+        return true;
+    }
+
+    /* Utility method for printing out empty checks. */
+    public static boolean checkSize(int expected, int actual) {
+        if (expected != actual) {
+            System.out.println("size() returned " + actual + ", but expected: " + expected);
+            return false;
+        }
+        return true;
+    }
+
+    public static boolean checkElements(String expected, String actual) {
+        boolean result = expected.equals(actual);
+
+        if (!result) {
+            System.out.println("Actual elements: " + actual + ", but expected: " + expected);
+        }
+
+        return result;
+    }
+
+    public static boolean checkIntEqual(Integer expected, Integer actual) {
+        boolean result = expected == null && actual == null || expected.equals(actual);
+
+        if (!result) {
+            System.out.println("Actual element: " + actual + ", but expected: " + expected);
+        }
+
+        return result;
+    }
+
+    /* Prints a nice message based on whether a test passed.
+     * The \n means newline. */
+    public static void printTestStatus(boolean passed) {
+        if (passed) {
+            System.out.println("Test passed!\n");
+        } else {
+            System.out.println("Test failed!\n");
+        }
+    }
+
+    /**
+     * Adds a few things to the list, checking isEmpty() and size() are correct,
+     * finally printing the results.
+     * <p>
+     * && is the "and" operation.
+     */
+    public static void addIsEmptySizeTest() {
+        System.out.println("Running add/isEmpty/Size test.");
+        ArrayDeque<String> ad1 = new ArrayDeque<String>();
+
+        boolean passed = checkEmpty(true, ad1.isEmpty());
+
+        ad1.addFirst("front");
+
+        // The && operator is the same as "and" in Python.
+        // It's a binary operator that returns true if both arguments true, and false otherwise.
+        passed = checkSize(1, ad1.size()) && passed;
+        passed = checkEmpty(false, ad1.isEmpty()) && passed;
+        passed = checkElements("front", ad1.getElements()) && passed;
+
+        ad1.addLast("middle");
+        passed = checkSize(2, ad1.size()) && passed;
+        passed = checkElements("front | middle", ad1.getElements()) && passed;
+
+        ad1.addLast("back");
+        passed = checkSize(3, ad1.size()) && passed;
+        passed = checkElements("front | middle | back", ad1.getElements()) && passed;
+
+        System.out.println("Printing out deque: ");
+        ad1.printDeque();
+
+        printTestStatus(passed);
+    }
+
+    /**
+     * Adds an item, then removes an item, and ensures that dll is empty afterwards.
+     */
+    public static void addRemoveTest() {
+
+        System.out.println("Running add/remove test.");
+
+		ArrayDeque<Integer> ad1 = new ArrayDeque<Integer>();
+		// should be empty 
+		boolean passed = checkEmpty(true, ad1.isEmpty());
+
+		ad1.addFirst(10);
+		// should not be empty 
+		passed = checkEmpty(false, ad1.isEmpty()) && passed;
+
+		ad1.removeFirst();
+		// should be empty 
+		passed = checkEmpty(true, ad1.isEmpty()) && passed;
+
+        ad1.addFirst(10);
+        ad1.addFirst(20);
+        ad1.addFirst(30);
+
+        ad1.removeFirst();
+        passed = checkElements("20 | 10", ad1.getElements()) && passed;
+
+        ad1.removeLast();
+        passed = checkElements("20", ad1.getElements()) && passed;
+
+        ad1.removeLast();
+        passed = checkElements("", ad1.getElements()) && passed;
+        passed = checkEmpty(true, ad1.isEmpty()) && passed;
+
+		printTestStatus(passed);
+    }
+
+    public static void getTest() {
+
+        System.out.println("Running get test.");
+
+        ArrayDeque<Integer> ad1 = new ArrayDeque<Integer>();
+
+        boolean passed = checkIntEqual(null, ad1.get(0));
+
+        ad1.addFirst(10);
+        ad1.addFirst(20);
+        ad1.addFirst(30);
+
+        passed = checkIntEqual(30, ad1.get(0)) && passed;
+        passed = checkIntEqual(10, ad1.get(2)) && passed;
+        passed = checkIntEqual(20, ad1.get(1)) && passed;
+        passed = checkIntEqual(null, ad1.get(4)) && passed;
+
+        printTestStatus(passed);
+    }
+
+    public static void circularDemoTest() {
+        // this test uses the values from the circular array deque slide
+        System.out.println("Running circular array deque demo test.");
+
+        ArrayDeque<String> ad1 = new ArrayDeque<String>();
+        ad1.addLast("a");
+        ad1.addLast("b");
+        ad1.addFirst("c");
+        ad1.addLast("d");
+        ad1.addLast("e");
+        ad1.addFirst("f");
+        ad1.addLast("g");
+        ad1.addLast("h");
+
+        boolean passed = checkElements("f | c | a | b | d | e | g | h", ad1.getElements());
+
+        ad1.addLast("Z");
+
+        passed = checkElements("f | c | a | b | d | e | g | h | Z", ad1.getElements()) && passed;
+
+        ad1.removeLast();
+        passed = checkElements("f | c | a | b | d | e | g | h", ad1.getElements()) && passed;
+
+        printTestStatus(passed);
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Running tests.\n");
+        addIsEmptySizeTest();
+        addRemoveTest();
+        getTest();
+
+        circularDemoTest();
+    }
+} 
