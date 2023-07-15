6fbc403a174f077248b0d3e3e1193b945d7d4a84
Felipe Lima <26683783+feslima@users.noreply.github.com>
2023-07-15 20:24:36 -0300

    feat: BSTMap deletion + iteration

--- lab9/lab9/BSTMap.java
+++ lab9/lab9/BSTMap.java
@@ -1,5 +1,7 @@
 package lab9;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Set;
 
@@ -132,9 +134,21 @@ public class BSTMap<K extends Comparable<K>, V> implements Map61B<K, V> {
     //////////////// EVERYTHING BELOW THIS LINE IS OPTIONAL ////////////////
 
     /* Returns a Set view of the keys contained in this map. */
+    private void inOrderTraversal(Node p, Set<K> keyset) {
+        if (p == null) {
+            return;
+        }
+
+        inOrderTraversal(p.left, keyset);
+        keyset.add(p.key);
+        inOrderTraversal(p.right, keyset);
+    }
+
     @Override
     public Set<K> keySet() {
-        throw new UnsupportedOperationException();
+        Set<K> keyset = new HashSet<>();
+        inOrderTraversal(root, keyset);
+        return keyset;
     }
 
     /**
@@ -144,7 +158,93 @@ public class BSTMap<K extends Comparable<K>, V> implements Map61B<K, V> {
      */
     @Override
     public V remove(K key) {
-        throw new UnsupportedOperationException();
+        Node removed = removeHelper(key, root, root);
+
+        if (removed != null) {
+            return removed.value;
+        }
+        return null;
+    }
+
+    private Node removeHelper(K key, Node p, Node parent) {
+        if (p == null) {
+            return null;
+        }
+
+        int cmp = p.key.compareTo(key);
+        if (cmp > 0) {
+            // this node key is greater than key
+            return removeHelper(key, p.left, p);
+        }
+
+        if (cmp < 0) {
+            // this node key is lesser than key
+            return removeHelper(key, p.right, p);
+        }
+
+        size -= 1;
+
+        Node toBeDeleted = new Node(p.key, p.value);
+        // if the node being deleted has no children, simply delete it
+        if (p.right == null && p.left == null) {
+            if (parent.left.key.compareTo(p.key) == 0) {
+                parent.left = null;
+            }
+            if (parent.right.key.compareTo(p.key) == 0) {
+                parent.right = null;
+            }
+            return toBeDeleted;
+        }
+
+        /* if the node being deleted has one child, delete the node and plug the child in the spot where the
+        deleted node was */
+        if (p.right != null && p.left == null) {
+            p.key = p.right.key;
+            p.value = p.right.value;
+            p.right = null;
+            return toBeDeleted;
+        }
+
+        if (p.left != null && p.right == null) {
+            p.key = p.left.key;
+            p.value = p.left.value;
+            p.left = null;
+            return toBeDeleted;
+        }
+
+        // if the node has two children, replace the node with the successor node
+        ArrayList<Node> successorAndParent = findSuccessorWithParent(p.right, p);
+        Node successor = successorAndParent.get(0);
+        Node successorParent = successorAndParent.get(1);
+        p.key = successor.key;
+        p.value = successor.value;
+        if (successorParent.left != null && successorParent.left.key.compareTo(successor.key) == 0) {
+            successorParent.left = null;
+        }
+        if (successorParent.right != null && successorParent.right.key.compareTo(successor.key) == 0) {
+            successorParent.right = null;
+        }
+        /* if the successor node has a right child, after plugging the successor node into the spot of the deleted node,
+         * take the former child of the successor node and turn into the left child of the former parent of the
+         * successor node
+         */
+        if (successor.right != null) {
+            successorParent.left = successor.right;
+        }
+
+        return toBeDeleted;
+    }
+
+    // Keep visiting the left child until there is no more child left, then return the childless node
+    private ArrayList<Node> findSuccessorWithParent(Node p, Node parent) {
+        if (p.left == null) {
+            ArrayList<Node> result = new ArrayList<Node>();
+            result.add(p);
+            result.add(parent);
+            return result;
+        }
+
+        return findSuccessorWithParent(p.left, p);
     }
 
     /**
@@ -159,6 +259,6 @@ public class BSTMap<K extends Comparable<K>, V> implements Map61B<K, V> {
 
     @Override
     public Iterator<K> iterator() {
-        throw new UnsupportedOperationException();
+        return keySet().iterator();
     }
 }
