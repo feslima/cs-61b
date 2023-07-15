789431185eec107fa56e504378d8598d6a86ff06
Felipe Lima <26683783+feslima@users.noreply.github.com>
2023-07-15 15:42:37 -0300

    feat: homework 2 (#6)
    * feat: initial Percolation drawing
    
    Does not make use of UnionFind yet
    
    * feat: percolation with union-find
    
    * feat: percolation stats class
    
    * fix: bugfix
    
    * fix: percolates bugfix
    
    * fix: percolation stats bug
    
    * fix: style and API
    
    * fix: percolation stats
    
    * fix: bug on percolation stats p* calculation
    
    * fix: fill open sites method
    
    * fix: another attempt...
    
    * fix: fill site behavior on open
    
    * Revert "fix: fill open sites method"
    
    This reverts commit ab81d2a821d796128b20d4473b050f7fab6a0da0.

--- hw2/hw2/Percolation.java
+++ hw2/hw2/Percolation.java
@@ -2,6 +2,200 @@ package hw2;
 
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
-public class Percolation {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Percolation {
+    private static class Site {
+        private final int x;
+        private final int y;
+
+        Site(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+
+    private final int BLOCKED = -1;
+    private final int EMPTY = 0;
+
+    private final int FULL = 1;
+    private final int gridSize;
+    private final int[] grid;
+    private final WeightedQuickUnionUF uf;
+
+    private int openSites;
+
+    public Percolation(int N) {
+        // create N-by-N grid, with all sites initially blocked
+        if (N <= 0) {
+            throw new IllegalArgumentException("N must be greater than 0.");
+        }
+
+        gridSize = N;
+        int nElements = gridSize * gridSize;
+
+        grid = new int[N * N];
+
+        for (int idx = 0; idx < nElements; idx++) {
+            grid[idx] = BLOCKED;
+        }
+        openSites = 0;
+
+        uf = new WeightedQuickUnionUF(nElements);
+    }
+
+    private int xyTo1d(int r, int c) {
+        return r * gridSize + c;
+    }
+
+    private void checkAccess(int row, int col) {
+        if (row < 0 || col < 0) {
+            throw new IndexOutOfBoundsException("Can't have negative index.");
+        }
+        if (row >= gridSize || col >= gridSize) {
+            throw new IndexOutOfBoundsException("Row/Column index too large.");
+        }
+    }
+
+    private void appendSiteIfValid(int row, int col, List<Site> siteList) {
+        try {
+            checkAccess(row, col);
+        } catch (IndexOutOfBoundsException ex) {
+            return;
+        }
+        siteList.add(new Site(row, col));
+    }
+
+    private List<Site> getNeighbors(int row, int col) {
+        ArrayList<Site> neighbors = new ArrayList<>();
+
+        appendSiteIfValid(row - 1, col, neighbors);
+        appendSiteIfValid(row + 1, col, neighbors);
+        appendSiteIfValid(row, col - 1, neighbors);
+        appendSiteIfValid(row, col + 1, neighbors);
+
+        return neighbors;
+    }
+
+    private List<Site> getEmptyNeighbors(int row, int col) {
+        ArrayList<Site> emptyNeighbors = new ArrayList<>();
+
+        for (Site site : getNeighbors(row, col)) {
+            if (isOpen(site.x, site.y) && !isFull(site.x, site.y)) {
+                emptyNeighbors.add(site);
+            }
+        }
+
+        return emptyNeighbors;
+    }
+
+    private void fillOpenSitesIfConnected(int row, int col) {
+        List<Site> neighbors = getEmptyNeighbors(row, col);
+        if (neighbors.size() == 0) {
+            return;
+        }
+
+        for (Site site : neighbors) {
+            int idx = xyTo1d(site.x, site.y);
+            grid[idx] = FULL;
+            fillOpenSitesIfConnected(site.x, site.y);
+        }
+    }
+
+    /*
+    Open the site (row, col) if it is not open already
+     */
+    public void open(int row, int col) {
+        checkAccess(row, col);
+
+        if (!isOpen(row, col)) {
+            openSites += 1;
+
+            int idx = xyTo1d(row, col);
+            if (row == 0) {
+                grid[idx] = FULL;
+            } else {
+                grid[idx] = EMPTY;
+            }
+
+
+            for (Site site : getNeighbors(row, col)) {
+                if (isOpen(site.x, site.y)) {
+                    int idxNeighbor = xyTo1d(site.x, site.y);
+                    uf.union(idx, idxNeighbor);
+
+                    for (int j = 0; j < gridSize; j++) {
+                        if (isFull(0, j) && uf.connected(j, idxNeighbor)) {
+                            grid[idxNeighbor] = FULL;
+                            fillOpenSitesIfConnected(site.x, site.y);
+                        }
+                    }
+                }
+            }
+        }
+
+    }
+
+    public boolean isOpen(int row, int col) {
+        checkAccess(row, col);
+        int value = grid[xyTo1d(row, col)];
+        return value != BLOCKED;
+    }
+
+    public boolean isFull(int row, int col) {
+        checkAccess(row, col);
+        return grid[xyTo1d(row, col)] == FULL;
+    }
+
+    public int numberOfOpenSites() {
+        return openSites;
+    }
+
+
+    private List<Site> getTopFilledRow() {
+        ArrayList<Site> filledSites = new ArrayList<>();
+
+        for (int j = 0; j < gridSize; j++) {
+            if (isFull(0, j)) {
+                filledSites.add(new Site(0, j));
+            }
+        }
+
+        return filledSites;
+    }
+
+    private List<Site> getBottomFilledRow() {
+        ArrayList<Site> filledSites = new ArrayList<>();
+
+        int row = gridSize - 1;
+        for (int j = 0; j < gridSize; j++) {
+            if (isFull(row, j)) {
+                filledSites.add(new Site(row, j));
+            }
+        }
+
+        return filledSites;
+    }
+
+    /* Does the system percolate? */
+    public boolean percolates() {
+        List<Site> topRow = getTopFilledRow();
+        List<Site> bottomRow = getBottomFilledRow();
+        if (topRow.size() == 0 || bottomRow.size() == 0) {
+            return false;
+        }
+
+        for (Site bottomSite : bottomRow) {
+            for (Site topSite : topRow) {
+                int idxTopSite = xyTo1d(topSite.x, topSite.y);
+                int idxBottomSite = xyTo1d(bottomSite.x, bottomSite.y);
+                if (uf.connected(idxTopSite, idxBottomSite)) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
 }
--- hw2/hw2/PercolationStats.java
+++ hw2/hw2/PercolationStats.java
@@ -1,5 +1,77 @@
 package hw2;
 
-public class PercolationStats {
+import edu.princeton.cs.introcs.StdRandom;
+import edu.princeton.cs.introcs.StdStats;
 
+import java.util.ArrayList;
+
+public class PercolationStats {
+    private final int T;
+    private final int N;
+    private final double[] thresholdValues;
+    private final PercolationFactory pf;
+
+    public PercolationStats(int N, int T, PercolationFactory pf) {
+        if (N <= 0 || T <= 0) {
+            throw new IllegalArgumentException("T and N must be greater than 0.");
+        }
+
+        this.N = N;
+        this.T = T;
+        this.pf = pf;
+        thresholdValues = new double[T];
+
+        for (int i = 0; i < T; i++) {
+            thresholdValues[i] = performExperiment();
+        }
+    }
+
+    private double performExperiment() {
+        ArrayList<Integer> sampledSites = new ArrayList<>();
+        for (int i = 0; i < N * N; i++) {
+            sampledSites.add(i);
+        }
+
+        Percolation percolation = pf.make(N);
+        while (!percolation.percolates()) {
+            int siteSampleIdx = StdRandom.uniform(sampledSites.size());
+            int siteIdx = sampledSites.remove(siteSampleIdx);
+            int row = getRow(siteIdx);
+            int col = getCol(siteIdx);
+
+            percolation.open(row, col);
+        }
+
+        return (double) percolation.numberOfOpenSites() / N;
+    }
+
+    private int getRow(int idx) {
+        return idx / N;
+    }
+
+    private int getCol(int idx) {
+        return idx % N;
+    }
+
+    public double mean() {
+        return StdStats.mean(thresholdValues);
+    }
+
+    public double stddev() {
+        return StdStats.stddev(thresholdValues);
+    }
+
+    public double confidenceLow() {
+        return mean() - 1.96 * stddev() / Math.sqrt(T);
+    }
+
+    public double confidenceHigh() {
+        return mean() + 1.96 * stddev() / Math.sqrt(T);
+    }
+
+    public static void main(String[] args) {
+        PercolationFactory pf = new PercolationFactory();
+        PercolationStats ps = new PercolationStats(50, 40, pf);
+        System.out.println("Mean: " + ps.mean());
+    }
 }
