ed3cbea6081503cce30236316342d5cf05e8ddb2
Felipe Lima <26683783+feslima@users.noreply.github.com>
2023-07-15 16:46:23 -0300

    feat: BSTMap implementation

--- lab9/lab9/BSTMap.java
+++ lab9/lab9/BSTMap.java
@@ -40,40 +40,93 @@ public class BSTMap<K extends Comparable<K>, V> implements Map61B<K, V> {
         size = 0;
     }
 
-    /** Returns the value mapped to by KEY in the subtree rooted in P.
-     *  or null if this map contains no mapping for the key.
+    /**
+     * Returns the value mapped to by KEY in the subtree rooted in P.
+     * or null if this map contains no mapping for the key.
      */
     private V getHelper(K key, Node p) {
-        throw new UnsupportedOperationException();
+        if (p == null) {
+            return null;
+        }
+
+        int cmp = p.key.compareTo(key);
+        if (cmp == 0) {
+            return p.value;
+        }
+
+        if (cmp > 0) {
+            // this node key is greater than key
+            return getHelper(key, p.left);
+        }
+
+        // this node key is lesser than key
+        return getHelper(key, p.right);
     }
 
-    /** Returns the value to which the specified key is mapped, or null if this
-     *  map contains no mapping for the key.
+    /**
+     * Returns the value to which the specified key is mapped, or null if this
+     * map contains no mapping for the key.
      */
     @Override
     public V get(K key) {
-        throw new UnsupportedOperationException();
+        return getHelper(key, root);
     }
 
-    /** Returns a BSTMap rooted in p with (KEY, VALUE) added as a key-value mapping.
-      * Or if p is null, it returns a one node BSTMap containing (KEY, VALUE).
+    /**
+     * Returns a BSTMap rooted in p with (KEY, VALUE) added as a key-value mapping.
+     * Or if p is null, it returns a one node BSTMap containing (KEY, VALUE).
      */
     private Node putHelper(K key, V value, Node p) {
-        throw new UnsupportedOperationException();
+        Node newNode = new Node(key, value);
+        if (size == 0) {
+            root = newNode;
+            size += 1;
+            return root;
+        }
+
+        int cmp = p.key.compareTo(key);
+        if (cmp == 0) {
+            p.value = value;
+        }
+
+        if (cmp > 0) {
+            // this node key is greater than key
+            if (p.left == null) {
+                // ready for insertion
+                p.left = newNode;
+                size += 1;
+                return newNode;
+            }
+
+            // keep looking
+            return putHelper(key, value, p.left);
+        }
+
+        // this node key is lesser than key
+        if (p.right == null) {
+            // ready for insertion
+            p.right = newNode;
+            size += 1;
+            return newNode;
+        }
+
+        // keep looking
+        return putHelper(key, value, p.right);
     }
 
-    /** Inserts the key KEY
-     *  If it is already present, updates value to be VALUE.
+    /**
+     * Inserts the key KEY
+     * If it is already present, updates value to be VALUE.
      */
     @Override
     public void put(K key, V value) {
-        throw new UnsupportedOperationException();
+        putHelper(key, value, root);
     }
 
     /* Returns the number of key-value mappings in this map. */
     @Override
     public int size() {
-        throw new UnsupportedOperationException();
+        return size;
     }
 
     //////////////// EVERYTHING BELOW THIS LINE IS OPTIONAL ////////////////
@@ -84,18 +137,20 @@ public class BSTMap<K extends Comparable<K>, V> implements Map61B<K, V> {
         throw new UnsupportedOperationException();
     }
 
-    /** Removes KEY from the tree if present
-     *  returns VALUE removed,
-     *  null on failed removal.
+    /**
+     * Removes KEY from the tree if present
+     * returns VALUE removed,
+     * null on failed removal.
      */
     @Override
     public V remove(K key) {
         throw new UnsupportedOperationException();
     }
 
-    /** Removes the key-value entry for the specified key only if it is
-     *  currently mapped to the specified value.  Returns the VALUE removed,
-     *  null on failed removal.
+    /**
+     * Removes the key-value entry for the specified key only if it is
+     * currently mapped to the specified value.  Returns the VALUE removed,
+     * null on failed removal.
      **/
     @Override
     public V remove(K key, V value) {
