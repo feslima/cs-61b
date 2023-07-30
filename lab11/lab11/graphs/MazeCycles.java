9a0f4e084e3d84b0eebeb59ea0efb3657b3bc90b
Felipe Lima <26683783+feslima@users.noreply.github.com>
2023-07-30 16:46:28 -0300

    feat: lab11 (#11)
    * feat: BFS implementation
    
    * feat: cycle detection

--- lab11/lab11/graphs/MazeCycles.java
+++ lab11/lab11/graphs/MazeCycles.java
@@ -1,7 +1,7 @@
 package lab11.graphs;
 
 /**
- *  @author Josh Hug
+ * @author Josh Hug
  */
 public class MazeCycles extends MazeExplorer {
     /* Inherits public fields:
@@ -9,6 +9,8 @@ public class MazeCycles extends MazeExplorer {
     public int[] edgeTo;
     public boolean[] marked;
     */
+    private boolean hasCycle = false;
+    private int[] invisibleEdgeTo;
 
     public MazeCycles(Maze m) {
         super(m);
@@ -16,9 +18,48 @@ public class MazeCycles extends MazeExplorer {
 
     @Override
     public void solve() {
-        // TODO: Your code here!
+        invisibleEdgeTo = new int[maze.N() * maze.N()];
+
+        for (int v = 0; v < maze.V(); v++) {
+            if (!marked[v] && !hasCycle()) {
+                dfs(v, -1);
+            }
+        }
     }
 
-    // Helper methods go here
+    private boolean hasCycle() {
+        return hasCycle;
+    }
+
+    private void dfs(int v, int parent) {
+        distTo[v] = v;
+        marked[v] = true;
+        announce();
+
+        for (int w : maze.adj(v)) {
+            if (hasCycle()) {
+                return;
+            }
+
+            if (!marked[w]) {
+                invisibleEdgeTo[w] = v;
+                dfs(w, v);
+            } else {
+                if (parent != w) {
+                    edgeTo[w] = v;
+                    announce();
+                    int x = v;
+                    while (x != w) {
+                        edgeTo[x] = invisibleEdgeTo[x];
+                        announce();
+                        x = invisibleEdgeTo[x];
+                    }
+                    hasCycle = true;
+                }
+            }
+
+        }
+    }
+
 }
 
