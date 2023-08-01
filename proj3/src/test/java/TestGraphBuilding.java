195dbcfceffd8498e1d40c9f310b0eaf31624303
Felipe Lima <26683783+feslima@users.noreply.github.com>
2023-08-01 20:27:12 -0300

    chore: remove berkeley-2018-small.osm.xml from git

--- proj3/src/test/java/TestGraphBuilding.java
+++ proj3/src/test/java/TestGraphBuilding.java
@@ -15,7 +15,7 @@ public class TestGraphBuilding {
 
     private static final String OSM_DB_PATH = "../library-sp18/data/berkeley-2018.osm.xml";
     private static final String OSM_DB_PATH_SMALL =
-            "./berkeley-2018-small.osm.xml";
+            "../library-sp18/data/berkeley-2018-small.osm.xml";
     private static boolean initialized = false;
 
     /**
